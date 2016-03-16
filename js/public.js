c8851bc22bbd78db031fad0d4511d6739b1a448a
Yu-Aaron <1217.yyl@gmail.com>
2016-03-16 11:17:43 +0800

    first commit

---
+++ js/public.js
@@ -0,0 +1,7 @@
+/*引入js，css文件*/
+document.write('<link href="css/bootstrap.min.css" rel="stylesheet">');
+document.write('<link href="iconfont/iconfont.css" rel="stylesheet">');
+document.write('<link href="css/reset.css" rel="stylesheet">');
+document.write('<link href="css/style.css" rel="stylesheet">');
+document.write('<script src="js/jquery-1.12.1.js"></script>');
+document.write('<script src="js/bootstrap.min.js"></script>');
